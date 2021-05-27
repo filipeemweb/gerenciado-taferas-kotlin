07412f65315d565c4667923d7e6731f8c9bc1561
Filipe Lacerda <filipeem.web@gmail.com>
2021-05-26 22:37:13 -0300

    Cria a primeira parte da API Rest com Kotlin e Spring

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# gerenciado-taferas-kotlin
+# API - Gerenciador de Tarefas em Kotlin com Spring
 API Backend para o projeto do Gerenciador de Tarefas do curso Devaria 2021
---
+++ src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/controllers/LoginController.kt
@@ -0,0 +1,32 @@
+package br.com.devaria.projetos.gerenciadortarefas.controllers
+
+import br.com.devaria.projetos.gerenciadortarefas.dtos.ErroDTO
+import br.com.devaria.projetos.gerenciadortarefas.dtos.LoginDTO
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.lang.RuntimeException
+
+@RestController
+@RequestMapping("api/login")
+class LoginController {
+
+    @PostMapping
+    fun efetuarLogin(@RequestBody dto : LoginDTO) : ResponseEntity<Any> {
+        try {
+            if (dto == null || dto.login.isNullOrBlank() || dto.login.isNullOrEmpty()
+                || dto.senha.isNullOrBlank() || dto.senha.isNullOrEmpty()) {
+                return ResponseEntity(ErroDTO(HttpStatus.BAD_REQUEST.value(),
+                    "Parâmetros de entrada inválidos"), HttpStatus.BAD_REQUEST)
+            }
+
+            return ResponseEntity("Login autenticado com sucesso", HttpStatus.OK)
+        } catch (e: Exception) {
+            return ResponseEntity(ErroDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Não foi possível efetuar o login, tente novamente"), HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+}
